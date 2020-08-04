b7d431c63d4bd0375eaf2260aae655f11b6d4d7a
Gamaliel Barron <gbarronmm@gmail.com>
2020-08-04 09:52:54 -0500

    initial server setup

---
+++ server/server/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const app = express();
+
+module.exports = (config) => {
+  const log = config.log();
+  // Add a request logging middleware in development mode
+  if (app.get('env') === 'development') {
+    app.use((req, res, next) => {
+      log.debug(`${req.method}: ${req.url}`);
+      return next();
+    });
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    // Log out the error to the console
+    log.error(error);
+    return res.json({
+      error: {
+        message: error.message,
+      },
+    });
+  });
+  return app;
+};
